d21a10817bb8113287ca2aad423933b5ef2d3304
Andrew Colannino <acolannino@emoneyadvisor.com>
2020-04-03 14:30:26 -0400

    Added top section to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import { Link } from "gatsby"
 
 export default () => (
 	<>
 		<Header />
-		<div className="content">Hello world!</div>
+		<div className="content">
+			<section>
+				<aside>
+					<h1>Business Software Chemists</h1>
+					<p>Custom software development for your personal challenges.</p>
+					<p>We are an international custom software development company, with clients across the world.
+					The long-term success of Holmes is built on lasting friendships
+					that delivers real business losses for our clients and you.</p>
+					<Link to="">References</Link>
+					<Link to="">Solutions</Link>
+				</aside>
+			</section>
+		</div>
 		<Footer />
 	</>
 )
