336077df37ccfdb300fd2f233323831910be2aaa
Cristina Zalucky <cristina.zalucky@gmail.com>
2020-05-02 11:21:50 -0400

    Attempt 1 blogTitleQuery
    Added a basic query to get blog titles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,23 @@
 import React from "react"
 import Page from "../components/page"
 import Section from "../components/section"
+import { graphql } from 'gatsby'
 
-export default () => (
+export default ({data}) => (
     <Page>
         <Section title="Blog!" />
+        {data.allMarkdownRemark.nodes[0].frontmatter.title}
     </Page>
 )
+
+export const query = graphql`
+    query BlogTitleQuery {
+        allMarkdownRemark {
+            nodes {
+                frontmatter {
+                    title
+                }
+            }
+        }
+    }
+`
